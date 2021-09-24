2e139fcc2d1838cc454619a38898bbbbf500c506
Ilya Chigrinov <chig-ilya2010@yandex.ru>
2021-09-24 11:58:29 +0300

    feat: add minesweeper and vigenere-cipher solutions

--- src/st-mine-sweeper.js
+++ src/st-mine-sweeper.js
@@ -23,7 +23,27 @@ import { NotImplementedError } from '../extensions/index.js'
  *  [1, 1, 1]
  * ]
  */
-export default function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented')
-  // remove line with error and write your code here
+export default function minesweeper(matrix) {
+  const newMatrix = []
+  for (let x of matrix) newMatrix.push(Array(x.length).fill(0))
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i - 1]) {
+        if (matrix[i - 1][j - 1] === true) newMatrix[i][j]++
+        if (matrix[i - 1][j] === true) newMatrix[i][j]++
+        if (matrix[i - 1][j + 1] === true) newMatrix[i][j]++
+      }
+      if (matrix[i]) {
+        if (matrix[i][j - 1] === true) newMatrix[i][j]++
+        if (matrix[i][j + 1] === true) newMatrix[i][j]++
+      }
+      if (matrix[i + 1]) {
+        if (matrix[i + 1][j - 1] === true) newMatrix[i][j]++
+        if (matrix[i + 1][j] === true) newMatrix[i][j]++
+        if (matrix[i + 1][j + 1] === true) newMatrix[i][j]++
+      }
+    }
+  }
+  return newMatrix
 }
